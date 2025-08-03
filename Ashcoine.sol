e9baeae5e942841dcdebdbcb131f1e5a3d5f4023
Hamit bull <153437987+hamitbull@users.noreply.github.com>
2025-08-03 08:13:50 +0100

    Create Ashcoine.sol

---
+++ Ashcoine.sol
@@ -0,0 +1,6 @@
+ParserError: Expected string end-quote.
+  --> Ashcoine.sol/Ashcoine1.sol:36:185:
+   |
+36 |  ... equire(balanceOf[_from] >= _value, "Not enough
+   |                                         ^^^^^^^^^^^
+
